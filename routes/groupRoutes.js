bc122e52bd446e735e266faca6356847d3a63899
Parag Debnath <paragforwork@gmail.com>
2026-01-25 01:28:49 +0530

    pushing groups

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlleware/authMiddleware');
-const { createGroup, getGroup } = require('../controller/groupController');
+const { createGroup, getGroup, getGroups } = require('../controller/groupController');
 
 router.post('/', authMiddleware, createGroup);
+router.get('/allgroups', authMiddleware, getGroups);
 router.get('/:id', authMiddleware, getGroup);
-
 module.exports = router;
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 
 const generateToken=(id)=>{
     return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'7d', // 7 days for better UX
+        expiresIn:'30m', // 7 days for better UX
     });
 }
 
